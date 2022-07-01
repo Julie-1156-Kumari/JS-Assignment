227e21aa61a5b3e6a2fcb4513ca5a24c2941a152
JulieKumari1156 <105566779+Julie-1156-Kumari@users.noreply.github.com>
2022-07-01 18:29:16 +0000

    test

--- Problem1.js
+++
@@ -1,8 +0,0 @@
-// PROBLEM1 : Find factorial of N
-
-const N = 5;
-for (let i = 1; i<=5; i++){
-  var factorial = 1;
-  factorial = factorial * i;
-}
-console.log("factorial value of 5 :", factorial);
---
+++ problem1.js
@@ -0,0 +1,8 @@
+// PROBLEM1 : Find factorial of N
+
+let f = 1;
+for (var n = 5; n >= 1; n--) {
+
+    f = f * n;
+}
+console.log("factorial value of 5=",f);
